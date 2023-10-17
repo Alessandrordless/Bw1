54e7bee721c26c6d7fd8c8d64a5115edc121efb2
Katiacrisci <katcrisci@gmail.com>
2023-10-17 17:38:39 +0200

    add javascript

--- Feedback Page/js/scrypt.js
+++ Feedback Page/js/scrypt.js
@@ -1,8 +1,26 @@
-const star1 = document.getElementByClass("star");
-const star2 = document.getElementByClass("star");
-star1.addEventListener("click", function (event) {
-  changeColor("blue");
-});
-star2.addEventListener("click", function (event) {
-  changeColor("green");
-});
+// const star1 = document.getElementByClass("star");
+
+const stars = Array.from(document.getElementsByTagName("i"));
+console.log(stars);
+const starContainer = document.querySelector(".stars");
+
+const highlightStars = (event) => {
+  const selectedStar = event.srcElement;
+  const nthStar = stars.indexOf(selectedStar);
+  stars.forEach((star) => star.remove());
+
+  for (let i = 0; i <= nthStar; i++) {
+    const img = document.createElement("img");
+    img.src = "../Welcome/Images/star.svg";
+    img.classList.add("star");
+    starContainer.appendChild(img);
+  }
+
+  for (let i = 0; i < 10 - nthStar - 1; i++) {
+    const i = document.createElement("i");
+    i.classList.add("fas", "fa-star", "star");
+    starContainer.appendChild(i);
+  }
+};
+
+stars.forEach((star) => star.addEventListener("click", highlightStars));
