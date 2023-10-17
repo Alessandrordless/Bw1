e8bd0c32e60b03298a5ae6736997988ab3299178
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2023-10-17 18:39:46 +0200

    js css stars

--- Feedback Page/js/scrypt.js
+++ Feedback Page/js/scrypt.js
@@ -7,19 +7,9 @@ const starContainer = document.querySelector(".stars");
 const highlightStars = (event) => {
   const selectedStar = event.srcElement;
   const nthStar = stars.indexOf(selectedStar);
-  stars.forEach((star) => star.remove());
 
   for (let i = 0; i <= nthStar; i++) {
-    const img = document.createElement("img");
-    img.src = "../Welcome/Images/star.svg";
-    img.classList.add("star");
-    starContainer.appendChild(img);
-  }
-
-  for (let i = 0; i < 10 - nthStar - 1; i++) {
-    const i = document.createElement("i");
-    i.classList.add("fas", "fa-star", "star");
-    starContainer.appendChild(i);
+    stars[i].classList.add("stars-on");
   }
 };
 
