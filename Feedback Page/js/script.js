028676233f601052a00aa84fece17bbc47e414df
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2023-10-20 15:27:48 +0200

    feedback page js

--- Feedback Page/js/script.js
+++ Feedback Page/js/script.js
@@ -3,10 +3,13 @@
 const stars = Array.from(document.getElementsByClassName("star"));
 console.log(stars);
 const starContainer = document.querySelector(".star");
+let nthStar;
+
 
 const highlightStars = (event) => {
+  event.preventDefault()
   const selectedStar = event.srcElement;
-  const nthStar = stars.indexOf(selectedStar);
+  nthStar = stars.indexOf(selectedStar)
 
   for (let i = 0; i <= nthStar; i++) {
     stars[i].classList.add("star");
@@ -14,19 +17,35 @@ const highlightStars = (event) => {
   }
 };
 
+
+
 stars.forEach((star) => star.addEventListener("click", highlightStars));
 
-const filteredElement = document.getElementsByClassName("star");
 
-filteredElement.addEventListener("click", filteredElement);
 
-filteredElement();
+// const filteredElement = document.getElementsByClassName("star");
 
-// const input = document.getElementsByTagName("input")[0];
-// const button = document.getElementById("btn");
+// filteredElement.addEventListener("click", highlightStars);
 
-// button.disabled = true;
+// filteredElement();
 
-// input.addEventListener("change", () => {
-//   button.toggleAttribute("disabled");
-// });
+
+const sendButton = document.getElementsByClassName("send-button")[0]
+console.log(sendButton)
+
+const changeText = function () {
+
+const pText = document.querySelector(".just-p p") 
+
+
+if (nthStar <= 5) {
+pText.innerText = "Sorry that you didn't enjoy Epicode."
+} else if (nthStar < 8) {
+  pText.innerText = "Thank you for your feedback."
+} else if (nthStar < 11 ) {
+  pText.innerText = "We are glad you had a good experience."
+}
+}
+
+sendButton.addEventListener("click", changeText)
+
